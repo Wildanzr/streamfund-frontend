5f8889403ea2b3aa359b533d1b5195176b25b149
Kevin Tanuwijaya <kevintanuwijayasao@gmail.com>
2024-11-08 03:46:30 +0700

    feat: handle empty support

--- components/support/SupportTable.tsx
+++ components/support/SupportTable.tsx
@@ -35,38 +35,48 @@ export default function SupportTable({
         </TableRow>
       </TableHeader>
       <TableBody>
-        {supports.map((support: SupportResponse) => (
-          <TableRow key={support._id}>
-            <TableCell className="whitespace-nowrap">{formatDate(support.creted_at)}</TableCell>
-            <TableCell>
-              <div className="flex flex-row gap-2 items-center">
-                <p>{trimAddress(support.from)}</p>
-                <CopyIcon
-                  onClick={() => handleCopyAddress(support.from)}
-                  color="gray"
-                  size={14}
-                  className="cursor-pointer"
-                />
-              </div>
-            </TableCell>
-            <TableCell>{support.message}</TableCell>
-            <TableCell className="whitespace-nowrap">
-              {formatUnits(BigInt(support.amount), support?.token?.decimal)}{" "}
-              {support?.token?.symbol}
-            </TableCell>
-            <TableCell>
-              <div className="flex flex-row gap-2 items-center">
-                <p>{trimAddress(support.hash)}</p>
-                <CopyIcon
-                  onClick={() => handleCopyAddress(support.hash)}
-                  color="gray"
-                  size={14}
-                  className="cursor-pointer"
-                />
-              </div>
+        {supports && supports.length === 0 ? (
+          <TableRow>
+            <TableCell colSpan={5} className="text-center py-10">
+              No supports received yet
             </TableCell>
           </TableRow>
-        ))}
+        ) : (
+          supports.map((support: SupportResponse) => (
+            <TableRow key={support._id}>
+              <TableCell className="whitespace-nowrap">
+                {formatDate(support.creted_at)}
+              </TableCell>
+              <TableCell>
+                <div className="flex flex-row gap-2 items-center">
+                  <p>{trimAddress(support.from)}</p>
+                  <CopyIcon
+                    onClick={() => handleCopyAddress(support.from)}
+                    color="gray"
+                    size={14}
+                    className="cursor-pointer"
+                  />
+                </div>
+              </TableCell>
+              <TableCell>{support.message}</TableCell>
+              <TableCell className="whitespace-nowrap">
+                {formatUnits(BigInt(support.amount), support?.token?.decimal)}{" "}
+                {support?.token?.symbol}
+              </TableCell>
+              <TableCell>
+                <div className="flex flex-row gap-2 items-center">
+                  <p>{trimAddress(support.hash)}</p>
+                  <CopyIcon
+                    onClick={() => handleCopyAddress(support.hash)}
+                    color="gray"
+                    size={14}
+                    className="cursor-pointer"
+                  />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))
+        )}
       </TableBody>
     </Table>
   );
