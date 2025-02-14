103d665435f0ea44d9b69d53f34a2220cc09218a
Danzrrr <graita.sukma@gmail.com>
2025-02-14 13:12:32 +0700

    feat: link to manta version

--- components/shared/WalletButton.tsx
+++ components/shared/WalletButton.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { useAccount } from "wagmi";
+import { Button } from "../ui/button";
 
 interface WalletButtonProps {
   label?: string;
@@ -21,7 +23,17 @@ const WalletButton = ({ label }: WalletButtonProps) => {
       }, 2000);
     }
   }, [pathname, router, status]);
-  return <ConnectButton label={label} />;
+  return (
+    <div className="flex flex-col space-y-1">
+      <ConnectButton label={label} />
+
+      <Link href="https://staging.streamfund.live" target="_blank">
+        <Button variant="link" className="text-white">
+          Manta Version -&gt;
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default WalletButton;
